39ba8a8dfa1875aa27ffd758277c8239cb40480f
Shamilprog <shamilprog@yandex.ru>
2021-01-08 22:31:01 +0300

    Create an own view for drawing.

---
+++ app/src/main/java/ru/shamilprog/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,18 @@
+package ru.shamilprog.android.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+
+    // Used when a view is being created programmatically
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when a view is being inflated according to XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
