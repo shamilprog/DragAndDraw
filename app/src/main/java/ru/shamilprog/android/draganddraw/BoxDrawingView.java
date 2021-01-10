69341fe5ea1be6aac0838e833f62971511fde22b
Shamilprog <shamilprog@yandex.ru>
2021-01-10 22:06:59 +0300

    Creating boxes while touching the screen but not drawing them just yet.

--- app/src/main/java/ru/shamilprog/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/ru/shamilprog/android/draganddraw/BoxDrawingView.java
@@ -7,8 +7,14 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
 
     // Used when a view is being created programmatically
     public BoxDrawingView(Context context) {
@@ -28,15 +34,24 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                // Reset current state
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
 
