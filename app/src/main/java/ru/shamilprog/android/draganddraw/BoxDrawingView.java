dd3a8bc576a2862c5815fa0ecddf6e29b56ad668
Shamilprog <shamilprog@yandex.ru>
2021-01-10 21:57:26 +0300

    Listening to touch events.

--- app/src/main/java/ru/shamilprog/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/ru/shamilprog/android/draganddraw/BoxDrawingView.java
@@ -1,10 +1,14 @@
 package ru.shamilprog.android.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
 
     // Used when a view is being created programmatically
     public BoxDrawingView(Context context) {
@@ -14,5 +18,30 @@ public class BoxDrawingView extends View {
     // Used when a view is being inflated according to XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+        return true;
     }
 }
--- app/src/main/java/ru/shamilprog/android/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/ru/shamilprog/android/draganddraw/DragAndDrawFragment.java
@@ -1,12 +1,13 @@
 package ru.shamilprog.android.draganddraw;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DragAndDrawFragment extends ListFragment {
+public class DragAndDrawFragment extends Fragment {
 
     public static DragAndDrawFragment newInstance() {
         return new DragAndDrawFragment();
